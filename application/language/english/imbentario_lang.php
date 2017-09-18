23f775c81b27324319a0382f01aaaa21cb37f6c6
SMTI-CKSagun <cristhiansagun@gmail.com>
2017-09-18 12:05:59 +0800

    TRAINING

--- application/language/english/imbentario_lang.php
+++ application/language/english/imbentario_lang.php
@@ -8,3 +8,16 @@ $lang['error_message']   = 'The quick brown fox jumps over the lazy dog.';
 $lang['warning_message'] = 'The quick brown fox jumps over the lazy dog.';
 
 $lang['try_again_message'] = 'Please Try Again!';
+
+$lang['add_department_success']        = 'Successfully added new department';
+$lang['edit_department_success']       = 'Successfully updated department';
+$lang['activate_department_success']   = 'Successfully activated department';
+$lang['deactivate_department_success'] = 'Successfully deactivated department';
+
+$lang['add_department_error']        = 'An error has occcured while adding new department. Please try again';
+$lang['edit_department_error']       = 'An error has occcured while editing department. Please try again';
+$lang['activate_department_error']   = 'An error has occcured while activating department. Please try again';
+$lang['deactivate_department_error'] = 'An error has occcured while deactivating department. Please try again';
+
+
+$lang['confirmation_message'] = "You're about to %s the record of %s.";
