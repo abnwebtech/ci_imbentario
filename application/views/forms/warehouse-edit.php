ad86437a7689bf4ef585b25dd37392eb674a3eb2
SMTI-RDaludado <rosemariedaludado@gmail.com>
2017-09-19 18:19:54 +0800

    BUILD-0007 DONE: Add and Edit- Required fields

--- application/views/forms/warehouse-edit.php
+++ application/views/forms/warehouse-edit.php
@@ -5,18 +5,21 @@
         <label class="control-label col-sm-3">Warehouse Name:</label>
         <div class="col-sm-7">
             <input class="form-control" type="text" name="name" value="<?php echo $warehouse['name']; ?>" placeholder="name">
+            <div class="validation_error"><?php echo form_error('name'); ?></div>
         </div>
     </div>
     <div class="form-group">
         <label class="control-label col-sm-3">Short Name:</label>
         <div class="col-sm-7">
             <input class="form-control" type="text" name="short_name" value="<?php echo $warehouse['short_name']; ?>" placeholder="short_name">
+            <div class="validation_error"><?php echo form_error('short_name'); ?></div>
         </div>
     </div>
     <div class="form-group">
         <label class="control-label col-sm-3">Address:</label>
         <div class="col-sm-7">
             <input class="form-control" type="text" name="address" value="<?php echo $warehouse['address']; ?>" placeholder="address">
+            <div class="validation_error"><?php echo form_error('address'); ?></div>
         </div>
     </div>
     <div class="form-group">
