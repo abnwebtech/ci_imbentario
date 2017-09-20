a4b62fbf049595edd85fda349b178e47a5c16c86
SMTI-RDaludado <rosemariedaludado@gmail.com>
2017-09-20 13:21:41 +0800

    BUILD-0007 DONE: View-Search function and Updated View for List of Warehouse

--- application/views/pages/warehouse-view.php
+++ application/views/pages/warehouse-view.php
@@ -1,49 +1,62 @@
+<script>
+$(function() {
+$('#datatable-warehouses').DataTable();
+});
+</script>
 <div class="row">
-    <div class="col-lg-10" >
-        <a class="btn btn-default" href="<?php echo site_url('warehouses/add'); ?>">ADD WAREHOUSE</a>
-        <!--<?php dump($positions); ?>
-        <?php echo $positions['modified']; ?>-->
-        <table class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th>ACTION</th>
-                    <!--<th>ID</th>-->
-                    <th>WAREHOUSE NAME</th>
-                    <th>SHORT NAME</th>
-                    <th>ADDRESS</th>
-                    <th>STATUS</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($warehouses as $index => $warehouse): ?>
-                <tr>
-                    <td>
-			            <a href="<?php echo site_url('warehouses/confirmation/edit/' . $warehouse['id']); ?>" data-toggle="modal" data-target="#modal-confirmation-<?php echo $index; ?>">
-                            <i class="fa fa-pencil"></i>
-                         	<span>Edit</span>
-                        </a>
-                        <a href="<?php echo site_url('warehouses/confirmation/' . $warehouse['status_url'] . '/' . $warehouse['id']); ?>" class="btn btn-link" data-toggle="modal" data-target="#modal-confirmation-<?php echo $index; ?>">
-                                    <i class="fa <?php echo $warehouse['status_icon']; ?>"></i> <?php echo $warehouse['status_action']; ?>
-                        </a>
-                    </td>
-                    <!--<td><?php echo $warehouse['id']; ?></td>-->
-                    <td><?php echo $warehouse['name']; ?></td>
-                    <td><?php echo $warehouse['short_name']; ?></td>
-                    <td><?php echo $warehouse['address']; ?></td>
-                    <td><?php echo $warehouse['status_label']; ?></td>
-                </tr>
-                            <div class="modal fade" id="modal-confirmation-<?php echo $index; ?>">
-                                <div class="modal-dialog">
-                                    <div class="modal-content"></div>
-                                </div>
-                            </div>
-			
-                <?php endforeach; ?>
-                            <?php if (empty($warehouses)): ?>
-                            <tr class="text-center">
-                                <td colspan="3">-- NO RECORD FOUND --</td>
-                            </tr>
-                            <?php endif; ?>
+    <div class="col-sm-12" >
+        <div class='box box-primary'>
+            <div class='box-header'>
+            <h3 class="box-title">List of Warehouse</h3>
+                <div class="box-tools">
+                    <a href="<?php echo site_url('warehouses/add'); ?>" class="btn btn-box-tool">
+                        <i class="fa fa-plus"></i>
+                        <span class="text-blue">Add New Warehouse</span>
+                    </a> 
+                </div> 
+            </div>              
+                <div class='box-body'>
+                <div class='table-responsive'>
+                <table  id="datatable-warehouses"  class='table table-bordered table-stripped table-hover'>
+                    <thead>
+                        <tr>
+                            <th>ACTION</th>
+                            <th>WAREHOUSE NAME</th>
+                            <th>SHORT NAME</th>
+                            <th>ADDRESS</th>
+                            <th class="text-center">STATUS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($warehouses as $index => $warehouse): ?>
+                        <tr>
+                            <td>
+                                <a href="<?php echo site_url('warehouses/confirmation/edit/' . $warehouse['id']); ?>" data-toggle="modal" data-target="#modal-confirmation-<?php echo $index; ?>">
+                                    <i class="fa fa-pencil"></i>
+                                    <span>Edit</span>
+                                </a>
+                                <a href="<?php echo site_url('warehouses/confirmation/' . $warehouse['status_url'] . '/' . $warehouse['id']); ?>" class="btn btn-link" data-toggle="modal" data-target="#modal-confirmation-<?php echo $index; ?>">
+                                            <i class="fa <?php echo $warehouse['status_icon']; ?>"></i> <?php echo $warehouse['status_action']; ?>
+                                </a>
+                            </td>
+                            <!--<td><?php echo $warehouse['id']; ?></td>-->
+                            <td><?php echo $warehouse['name']; ?></td>
+                            <td><?php echo $warehouse['short_name']; ?></td>
+                            <td><?php echo $warehouse['address']; ?></td>
+                            <td class="text-center"><?php echo $warehouse['status_label']; ?></td>
+                        </tr>
+                                    <div class="modal fade" id="modal-confirmation-<?php echo $index; ?>">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content"></div>
+                                        </div>
+                                    </div>
+                    
+                        <?php endforeach; ?>
+                                    <?php if (empty($warehouses)): ?>
+                                    <tr class="text-center">
+                                        <td colspan="3">-- NO RECORD FOUND --</td>
+                                    </tr>
+                                    <?php endif; ?>
 
             </tbody>
         </table>
